94684ebe0a347e90498b6094be8546ea066809b4
PixelsTownTechnologies <pixelstown@hotmail.com>
2021-02-13 13:42:10 +0200

    Init 3

---
+++ front-end/src/components/dashboard/user/login.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+interface LoginProps {
+}
+
+interface LoginState {
+}
+
+class LoginPage extends React.Component<LoginProps, LoginState> {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+
+}
+
+export default LoginPage;
